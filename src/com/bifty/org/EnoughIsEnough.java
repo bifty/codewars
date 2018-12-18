60850d697a14a5fbbff55874ee1940bdf4d58e13
Sebastian Geiger <sngr@methodpark.de>
2018-12-18 13:48:58 +0100

    updated to static function and inserted codewars test cases

--- src/com/bifty/org/EnoughIsEnough.java
+++ src/com/bifty/org/EnoughIsEnough.java
@@ -1,16 +1,14 @@
 package com.bifty.org;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class EnoughIsEnough {
 
-	public int[] deleteNth(int[] list, int maxOccurence) {
+	public static int[] deleteNth(int[] elements, int maxOccurrences) {
 		List<Integer> resultList;
-		List<Integer> pictureList =  Arrays.stream(list).boxed().collect(Collectors.toList());
-
+		List<Integer> pictureList =  Arrays.stream(elements).boxed().collect(Collectors.toList());
 
 		resultList = pictureList.stream().distinct().collect(Collectors.toList());
 		

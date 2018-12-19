ae6a5be22bb515b2cd32a26bac88087f21eaa563
Sebastian Geiger <sngr@methodpark.de>
2018-12-19 13:17:53 +0100

    EnoughIsEnough final

--- src/com/bifty/org/EnoughIsEnough.java
+++ src/com/bifty/org/EnoughIsEnough.java
@@ -1,17 +1,27 @@
 package com.bifty.org;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class EnoughIsEnough {
 
 	public static int[] deleteNth(int[] elements, int maxOccurrences) {
-		List<Integer> resultList;
+		final List<Integer> resultList = new ArrayList<Integer>();
+		Map<Integer, Integer> elementCounter = new HashMap<>();
 		List<Integer> pictureList =  Arrays.stream(elements).boxed().collect(Collectors.toList());
 
-		resultList = pictureList.stream().distinct().collect(Collectors.toList());
-		
+		pictureList.forEach(element -> {
+			int count = elementCounter.containsKey(element) ? elementCounter.get(element) : 0;
+			if (count < maxOccurrences) {
+				resultList.add(element);
+				elementCounter.put(element, count + 1);
+			}
+		});
+				
 		return resultList.stream().mapToInt((Integer i)->i).toArray();
 	}
 

b4173aa836b21812d1d9e076ade856eb86be2af4
Sebastian Geiger <sngr@methodpark.de>
2019-03-01 16:32:28 +0100

    JadenCase - other solution

--- src/com/bifty/org/JadenCase.java
+++ src/com/bifty/org/JadenCase.java
@@ -1,20 +1,29 @@
 package com.bifty.org;
 
+import java.util.Arrays;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class JadenCase {
 
-	public String toJadenCase(String phrase) {
-		if (phrase == null || phrase.equals("")) return null;
-		
-		String[] words = phrase.split(" ");
-		String newPhrase = "";
-		
-		for (String word : words) {
-			String newBegining = word.substring(0,1).toUpperCase();
-			String newWord = newBegining.concat(word.substring(1, word.length()));
-			newPhrase += newWord + " ";
-		}
-		
-		return newPhrase.trim();
-	}
+	  public static final Collector<CharSequence,?,String> JOIN_WITH_SPACE = Collectors.joining(" ");
+
+	  public String toJadenCase(String phrase) {
+	    if (phraseIsEmpty(phrase)) { return null; }
+	    
+	    return Arrays.stream(splitIntoWords(phrase)).map(this::capitalize).collect(JOIN_WITH_SPACE);
+	  }
+
+	  private String[] splitIntoWords(String phrase) {
+	    return phrase.split(" ");
+	  }
+	  
+	  private String capitalize(String word) {
+	    return word.substring(0, 1).toUpperCase() + word.substring(1);
+	  }
+	  
+	  private boolean phraseIsEmpty(String phrase) {
+	    return phrase == "" || phrase == null;
+	  }
 
 }
